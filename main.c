0b4d6f5b96bde39090417bb544e8b903fe273d6e
mark-use-blender <markyoutub00@gmail.com>
2024-10-18 12:38:43 +0800

    sine wave generation succes

--- main.c
+++ main.c
@@ -20,21 +20,21 @@ openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program pico-i
 #include "hardware/irq.h"
 #include "hardware/dma.h"
 #include "hardware/spi.h"
-#include "ph0.pio.h"
-int sinfeq = 60;
-uint16_t *buff;
+int sinfeq = 3000;
+uint16_t buff[5];
 
 
 
 void buffprep(int feq,int pha)
 {
-    uint16_t binfeq = (int)((feq)*(2<<27)/(25000000));
-    uint16_t binpha = (int)(pha*0.33*4096);
-    buff[0]=0x2100;
-    buff[1]=(0b01<<14)+(binfeq);
-    buff[2]=(0b01<<14);
-    buff[3]=(0xc000)+(binpha);
-    buff[4]=(0x2000);
+    uint32_t binfeq = (int)(round((((2<<27))/(1000000))*(feq)));
+    uint16_t binpha = (int)(round(pha*0.33*4096));
+    buff[0]=(0x3000);
+    buff[1]=((0x4000)+((binfeq)&(0x3fff)));
+    buff[2]=((0x4000)+((binfeq>>14)&(0x3fff)));
+    buff[3]=(0x3000);
+    buff[4]=((0xc000)+((binpha)&(0x0fff)));
+    
 
 
 
@@ -54,44 +54,48 @@ int main()
     gpio_set_function(18, GPIO_FUNC_SPI);
     gpio_set_function(19, GPIO_FUNC_SPI);
 
-    gpio_init(0);
+    //gpio_init(0);
     gpio_set_outover (1, GPIO_OVERRIDE_HIGH);
-    gpio_init(4);
+    //gpio_init(4);
     gpio_set_outover (5, GPIO_OVERRIDE_HIGH);
-    gpio_init(16);
+    //gpio_init(16);
     gpio_set_outover (17, GPIO_OVERRIDE_HIGH);
 
     
 
-    spi_init(spi0,20000000);
-    spi_set_format(spi0,16,0,1,1);
+    spi_init(spi0,20000);
+    spi_set_format(spi0,16,1,0,1);
     sleep_ms(1000);
     buffprep(sinfeq,0);
-    gpio_set_outover (1, GPIO_OVERRIDE_NORMAL);
+    gpio_set_outover (1, GPIO_OVERRIDE_LOW);
     spi_write16_blocking(spi0,buff,5);
     gpio_set_outover (1, GPIO_OVERRIDE_HIGH);
     buffprep(sinfeq,1);
-    gpio_set_outover (5, GPIO_OVERRIDE_NORMAL);
+    gpio_set_outover (5, GPIO_OVERRIDE_LOW);
     spi_write16_blocking(spi0,buff,5);
     gpio_set_outover (5, GPIO_OVERRIDE_HIGH);
     buffprep(sinfeq,2);
-    gpio_set_outover (17, GPIO_OVERRIDE_NORMAL);
+    gpio_set_outover (17, GPIO_OVERRIDE_LOW);
     spi_write16_blocking(spi0,buff,5);
     gpio_set_outover (17, GPIO_OVERRIDE_HIGH);
     while (true)
     {
+        //*
         buffprep(sinfeq,0);
-        gpio_set_outover (1, GPIO_OVERRIDE_NORMAL);
+        gpio_set_outover (1, GPIO_OVERRIDE_LOW);
         spi_write16_blocking(spi0,buff,5);
         gpio_set_outover (1, GPIO_OVERRIDE_HIGH);
         buffprep(sinfeq,1);
-        gpio_set_outover (5, GPIO_OVERRIDE_NORMAL);
+        gpio_set_outover (5, GPIO_OVERRIDE_LOW);
         spi_write16_blocking(spi0,buff,5);
         gpio_set_outover (5, GPIO_OVERRIDE_HIGH);
+        //*/
         buffprep(sinfeq,2);
-        gpio_set_outover (17, GPIO_OVERRIDE_NORMAL);
+        gpio_set_outover (17, GPIO_OVERRIDE_LOW);
         spi_write16_blocking(spi0,buff,5);
         gpio_set_outover (17, GPIO_OVERRIDE_HIGH);
+        sleep_ms(1000);
+        //*/
         tight_loop_contents();
         
         
