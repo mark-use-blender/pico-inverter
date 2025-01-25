3a4af3ba9b9a483085aa3f01154734e124992847
mark-use-blender <markyoutub00@gmail.com>
2025-01-25 16:37:44 +0800

    integration of HMI and DDS

--- main.c
+++ main.c
@@ -26,13 +26,15 @@ openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program pico-i
 #include "GUI_Paint.h"
 #include "ImageData.h"
 #include "Infrared.h"
-
+uint16_t buff[5];
+uint16_t tbuff[5];
 
 bool reserved_addr(uint8_t addr) {
 return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
 int sinfeq = 30;
 bool enable =0;
+bool stup = 0;
 bool bouncec =0;
 bool bouncel =0;
 bool bouncer =0;
@@ -58,14 +60,14 @@ void cre1()
         exit(0);
     }
     // /*1.Create a new image cache named IMAGE_RGB and fill it with white*/
-    Paint_NewImage((UBYTE *)BlackImage,LCD_1IN3.WIDTH,LCD_1IN3.HEIGHT, 0, WHITE);
+    Paint_NewImage((UBYTE *)BlackImage,LCD_1IN3.WIDTH,LCD_1IN3.HEIGHT,180, WHITE);
     Paint_SetScale(65);
     Paint_Clear(WHITE);
     Paint_SetRotate(ROTATE_0);
     Paint_Clear(WHITE);
 
 
-/*
+//*
     uint8_t up = 14;
 	uint8_t down = 20;
 	uint8_t left = 16;
@@ -73,7 +75,7 @@ void cre1()
 	uint8_t ctrl = 15;
 //*/
 
-//*
+/*
     uint8_t up = 2;
 	uint8_t down = 18;
 	uint8_t left = 16;
@@ -180,13 +182,107 @@ void cre1()
     }
 
 }
-void main()
+
+
+
+void buffprepsy()
 {
+    buff[0]=(0x2000);
+}
+
+void buffprepsi(int feq,int pha)
+{
+    uint32_t binfeq = (int)(round((((2<<27))/(1000000))*(feq)));
+    uint16_t binpha = (int)((4096)-(round((((pha)*(0.333333))*(4096)))));
+    buff[0]=(0x2100);
+    buff[1]=((0x4000)|((binfeq)&(0x3fff)));
+    buff[2]=((0x4000)|((binfeq>>14)&(0x3fff)));  
+    buff[3]=((0xc000)|((binpha)&(0x0fff)));
+}
+
+void wrtodds(int sinfeq)
+{
+        //program DDS individually
+        buffprepsi(sinfeq,0);
+        gpio_clr_mask(0x00000002);
+        spi_write16_blocking(spi0,buff,4);
+        gpio_set_mask(0x00020022);
+
+        buffprepsi(sinfeq,1);
+        gpio_clr_mask(0x00000020);
+        spi_write16_blocking(spi0,buff,4);
+        gpio_set_mask(0x00020022);
+
+        buffprepsi(sinfeq,2);
+        gpio_clr_mask(0x00020000);
+        spi_write16_blocking(spi0,buff,4);
+        gpio_set_mask(0x00020022);
+        
+        //enable all DDS at ones to achieve synchronisation
+        buffprepsy();
+        gpio_clr_mask(0x00020022);
+        spi_write16_blocking(spi0,buff,1);
+        gpio_set_mask(0x00020022);
+        
+}
+
+int main()
+{   
     multicore_launch_core1(cre1);
-    while(true)
+    gpio_init(0);
+    gpio_set_dir(0,true);
+    gpio_put(0,false);
+
+    gpio_init(1);
+    gpio_set_dir(1,true);
+    gpio_set_function( 2, GPIO_FUNC_SPI);
+    gpio_set_function( 3, GPIO_FUNC_SPI);
+    gpio_init(5);
+    gpio_set_dir(5,true);
+    gpio_set_function( 6, GPIO_FUNC_SPI);
+    gpio_set_function( 7, GPIO_FUNC_SPI);
+    gpio_init(17);
+    gpio_set_dir(17,true);
+    gpio_set_function(18, GPIO_FUNC_SPI);
+    gpio_set_function(19, GPIO_FUNC_SPI);
+
+    gpio_set_mask(0x00020022);
+
+    spi_init(spi0,200000);
+    spi_set_format(spi0,16,1,0,1);
+/*
+    sleep_ms(1000);
+    wrtodds(sinfeq);
+    sleep_ms(500);
+    gpio_put(0,true);
+//*/
+    while (true)
     {
-        sinfeq--;
-        sleep_ms(500);
+        //*
+        if (enable|!stup)
+        {
+            gpio_put(0,false);
+            wrtodds(sinfeq);
+            sleep_ms(500);
+            gpio_put(0,true);
+            stup = true;
+        }
+        if (!enable|stup)
+        {
+            gpio_put(0,false);
+            stup = false;
+        }
+        //*/
+        tight_loop_contents();
+        
+        
     }
+
+
+
+
+
+
+
 
 }
