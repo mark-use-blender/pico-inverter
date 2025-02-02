826b84bb5793bb01546236147802b69db6d5a291
mark-use-blender <markyoutub00@gmail.com>
2025-02-02 19:54:39 +0800

    clean up

--- main.c
+++ main.c
@@ -41,24 +41,17 @@ return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 void cre1()
 {
     DEV_Delay_ms(100);
-    //printf("LCD_1in3_test Demo\r\n");
     if(DEV_Module_Init()!=0){
         return;
     }
     DEV_SET_PWM(50);
-    /* LCD Init */
-    //printf("1.3inch LCD demo...\r\n");
     LCD_1IN3_Init(HORIZONTAL);
     LCD_1IN3_Clear(WHITE);
-    
-    //LCD_SetBacklight(1023);
     UDOUBLE Imagesize = LCD_1IN3_HEIGHT*LCD_1IN3_WIDTH*2;
     UWORD *BlackImage;
     if((BlackImage = (UWORD *)malloc(Imagesize)) == NULL) {
-        //printf("Failed to apply for black memory...\r\n");
         exit(0);
     }
-    // /*1.Create a new image cache named IMAGE_RGB and fill it with white*/
     Paint_NewImage((UBYTE *)BlackImage,LCD_1IN3.WIDTH,LCD_1IN3.HEIGHT, 180, WHITE);
     Paint_SetScale(65);
     Paint_Clear(WHITE);
@@ -72,38 +65,18 @@ right=21
 ctrl=15
 */
 
-//*
     uint8_t up = 20;
 	uint8_t down = 14;
 	uint8_t left = 21;
 	uint8_t right = 16;
 	uint8_t ctrl = 15;
-//*/
-
-/*
-    uint8_t up = 18;
-	uint8_t down = 2;
-	uint8_t left = 20;
-	uint8_t right = 16;
-	uint8_t ctrl = 3;
-//*/
-
-		 
 	SET_Infrared_PIN(up);
     SET_Infrared_PIN(down);
     SET_Infrared_PIN(left);
     SET_Infrared_PIN(right);
     SET_Infrared_PIN(ctrl);
 
-    
     Paint_Clear(WHITE);
-
-    // Paint_DrawRectangle(60, 60, 91, 90, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    // Paint_DrawRectangle(60, 150, 91, 180, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    // Paint_DrawRectangle(15, 105, 46, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    // Paint_DrawRectangle(105, 105, 136, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    // Paint_DrawRectangle(60, 105, 91, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    // LCD_1IN3_Display(BlackImage);
     while(true)
     {
         Paint_Clear(WHITE);
@@ -113,89 +86,45 @@ ctrl=15
         Paint_DrawRectangle(105, 105, 136, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
         Paint_DrawRectangle(60, 105, 91, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
         Paint_DrawNum(0,0,sinfeq,&Font20,0, 0x000f, 0xfff0);
-        // LCD_1IN3_DisplayWindows(0, 0,90, 20,BlackImage);
         Paint_DrawNum(0,20,step,&Font20,0, 0x000f, 0xfff0);
-        // LCD_1IN3_DisplayWindows(0, 20,90, 40,BlackImage);
-        // LCD_1IN3_Display(BlackImage);
-        if (enable){
-            Paint_DrawRectangle(90, 0, 110, 20, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            //LCD_1IN3_DisplayWindows(90, 0, 110, 20,BlackImage);
-        }
+        if (enable) Paint_DrawRectangle(90, 0, 110, 20, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
         else
         {
             Paint_DrawRectangle(90, 0, 110, 20, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            //LCD_1IN3_DisplayWindows(90, 0, 110, 20,BlackImage);
             if(DEV_Digital_Read(up ) == 0){
                 Paint_DrawRectangle(60, 60, 90, 90, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(60, 60, 90, 90,BlackImage);
-                // printf("gpio =%d\r\n",up);
                 if ((sinfeq+step)<1000) sinfeq=sinfeq+step;
                 else sinfeq = 1000;
                 DEV_Delay_ms(100);
             }
-            else{
-                //Paint_DrawRectangle(60, 60, 90, 90, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(60, 60, 90, 90,BlackImage);
-            }
-
             if(DEV_Digital_Read(down ) == 0){
                 Paint_DrawRectangle(60, 150, 90, 180, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(60, 150, 90, 180,BlackImage);
-                // printf("gpio =%d\r\n",down);
                 if ((sinfeq-step)>10) sinfeq=sinfeq-step;
                 else sinfeq=10;
                 DEV_Delay_ms(100);
             }
-            else{
-                //Paint_DrawRectangle(60, 150, 90, 180, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(60, 150, 90, 180,BlackImage);
-            }
-            
             if(DEV_Digital_Read(left ) == 0){
                 Paint_DrawRectangle(15, 105, 45, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(15, 105, 45, 135,BlackImage);
-                // printf("gpio =%d\r\n",left);
                 if (!bouncel)if (step<100) step=step*10;
                 bouncel=1;
             }
-            else{
-                //Paint_DrawRectangle(15, 105, 45, 135, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(15, 105, 45, 135,BlackImage);
-                bouncel=0;
-            }
-                
+            else bouncel=0;
             if(DEV_Digital_Read(right ) == 0){
                 Paint_DrawRectangle(105, 105, 135, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(105, 105, 135, 135,BlackImage);
-                // printf("gpio =%d\r\n",right);
                 if (!bouncer)if (step>1) step=step/10;
                 bouncer=1;
             }
-            else{
-                //Paint_DrawRectangle(105, 105, 135, 135, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-                //LCD_1IN3_DisplayWindows(105, 105, 135, 135,BlackImage);
-                bouncer=0;
-            }
-            // LCD_1IN3_Display(BlackImage);
+            else bouncer=0;
         }
         if(DEV_Digital_Read(ctrl ) == 0){
             Paint_DrawRectangle(60, 105, 90, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            //LCD_1IN3_DisplayWindows(60, 105, 90, 135,BlackImage);
-            // printf("gpio =%d\r\n",ctrl);
             if (!bouncec)enable = !enable;
             bouncec=1;
         }
-        else{
-            //Paint_DrawRectangle(60, 105, 90, 135, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            //LCD_1IN3_DisplayWindows(60, 105, 90, 135,BlackImage);
-            bouncec=0;
-        }
+        else bouncec=0;
         LCD_1IN3_Display(BlackImage);
     }
-
 }
-
-
 
 void buffprepsy()
 {
@@ -229,22 +158,18 @@ void wrtodds(int sinfeq)
         gpio_clr_mask(0x00020000);
         spi_write16_blocking(spi0,buff,4);
         gpio_set_mask(0x00020022);
-        
         //enable all DDS at ones to achieve synchronisation
         buffprepsy();
         gpio_clr_mask(0x00020022);
         spi_write16_blocking(spi0,buff,1);
         gpio_set_mask(0x00020022);
-        
 }
 
 int main()
 {   
-    
     gpio_init(0);
     gpio_set_dir(0,true);
     gpio_set_outover(0,GPIO_OVERRIDE_LOW);
-    //gpio_put(0,false);
 
     gpio_init(1);
     gpio_set_dir(1,true);
@@ -273,51 +198,31 @@ int main()
 //*/
     while (true)
     {
-        //gpio_set_outover(0,GPIO_OVERRIDE_LOW);
-        // gpio_put(0,false);
-
-        //*
         if ((enable)&&(!stup))
         {
             gpio_set_outover(0,GPIO_OVERRIDE_LOW);
-    //gpio_put(0,false);
             wrtodds(sinfeq);
             sleep_ms(500);
             gpio_set_outover(0,GPIO_OVERRIDE_HIGH);
-    //gpio_put(0,true);
             stup = true;
             continue;
         }
         else if ((!enable)&&(stup))
         {
             gpio_set_outover(0,GPIO_OVERRIDE_LOW);
-    //gpio_put(0,false);
             stup = false;
             continue;
         }
         else if (enable&&stup)
         {
             gpio_set_outover(0,GPIO_OVERRIDE_HIGH);
-    //gpio_put(0,true);
             continue;
         }
         else if ((!enable)&&(!stup))
         {
             gpio_set_outover(0,GPIO_OVERRIDE_LOW);
-    //gpio_put(0,false);
-            
         }
-        //*/
+        
         tight_loop_contents();
-        
-        
     }
-
-
-
-
-
-
-
-
 }
